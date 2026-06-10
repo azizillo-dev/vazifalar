7dc81743bf1836a098f072f4946c7ccb2d7ee22a
Azizillo <azizillonabiyev52@gmail.com>
2026-06-10 11:51:13 +0500

    dastlabki 8 ta masala

---
+++ list_comprehension.py
@@ -0,0 +1,133 @@
+# # 1-masala
+# data = [i for i in range(1,21)]
+# print(data)
+
+
+# # 2-masala
+# data = [i**2 for i in range(1,11)]
+# print(data)
+
+
+# # 3-masala
+# data = [i for i in range(1,31) if i % 2 ==0]
+# print(data)
+
+
+# # 4-masala
+# data = ["ali", "vali", "hasan"] 
+# new = [i.title() for i in data]
+# print(new)
+
+
+# # 5-masala
+# data = [1, 2, 3, 4, 5]
+# print([str(i) for i in data])
+
+
+
+# # 6-masala
+# print([i**2 for i in range(1,21) if i % 2==1])
+
+
+# 7-masala
+# data = [-5, 10, -3, 7, 0, 12] 
+# new = [i for i in data if i > 0]
+# print(new)
+
+
+# # 8-masala
+# data = ["Ali", "Muhammad", "Vali", "Abdulloh"]
+# new = [i for i in data if len(i) > 4]
+# print(new)
+
+
+
+# 9-masala
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
