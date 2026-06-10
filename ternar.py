0759f2a28e1204aa03f7a2c5c51d535eb9474480
Azizillo <azizillonabiyev52@gmail.com>
2026-06-10 09:23:37 +0500

    4 ta masala ishlandi

--- ternar.py
+++ ternar.py
@@ -1,12 +1,20 @@
-n = int(input("n = "))
+# # 1-masala
+# n = int(input("n = "))
+# print("juft" if n % 2 == 0 else "toq")
 
-print("juft" if n % 2 == 0 else "toq")
+# # 2-masala
+# n = int(input("n = "))
+# print("musbat" if n > 0 else "manfiy" if n < 0 else "nol")
 
+# # 3-masala
+# yosh = int(input("yosh = "))
+# print("voyaga yetmagan" if yosh < 18 else "voyaga yetgan")
 
+# 4-masala
+a = int(input("a = "))
+b = int(input("b = "))
 
-
-
-
+print(a if a > b else b) 
 
 
 
