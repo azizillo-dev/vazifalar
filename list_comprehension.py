326d10934830124efe39f7bcdf66be41cb6a6e8f
Azizillo <azizillonabiyev52@gmail.com>
2026-06-10 14:42:44 +0500

    List comprehension

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,80 +1,80 @@
-# # 1-masala
-# data = [i for i in range(1,21)]
-# print(data)
+# 1-masala
+data = [i for i in range(1,21)]
+print(data)
 
 
-# # 2-masala
-# data = [i**2 for i in range(1,11)]
-# print(data)
+# 2-masala
+data = [i**2 for i in range(1,11)]
+print(data)
 
 
-# # 3-masala
-# data = [i for i in range(1,31) if i % 2 ==0]
-# print(data)
+# 3-masala
+data = [i for i in range(1,31) if i % 2 ==0]
+print(data)
 
 
-# # 4-masala
-# data = ["ali", "vali", "hasan"] 
-# new = [i.title() for i in data]
-# print(new)
+# 4-masala
+data = ["ali", "vali", "hasan"] 
+new = [i.title() for i in data]
+print(new)
 
 
-# # 5-masala
-# data = [1, 2, 3, 4, 5]
-# print([str(i) for i in data])
-
-
-
-# # 6-masala
-# print([i**2 for i in range(1,21) if i % 2==1])
-
-
-# 7-masala
-# data = [-5, 10, -3, 7, 0, 12] 
-# new = [i for i in data if i > 0]
-# print(new)
-
-
-# # 8-masala
-# data = ["Ali", "Muhammad", "Vali", "Abdulloh"]
-# new = [i for i in data if len(i) > 4]
-# print(new)
+# 5-masala
+data = [1, 2, 3, 4, 5]
+print([str(i) for i in data])
 
 
 
-# # 9-masala
-# data = [("juft" if i % 2 ==0 else "toq" ) for i in range(1,11)]
-# print(data)
+# 6-masala
+print([i**2 for i in range(1,21) if i % 2==1])
 
 
-# # 10-masala
-# data = [100000, 250000, 50000, 300000]
-# new = [(i * 1.1 if i > 200000 else i*1) for i in data]
-# print(new)
+7-masala
+data = [-5, 10, -3, 7, 0, 12] 
+new = [i for i in data if i > 0]
+print(new)
 
 
-# # 11-masala
-# new = [(i, j) for i in range(1, 4) for j in range(1, 5)]
-# print(new)
+# 8-masala
+data = ["Ali", "Muhammad", "Vali", "Abdulloh"]
+new = [i for i in data if len(i) > 4]
+print(new)
+
+
+
+# 9-masala
+data = [("juft" if i % 2 ==0 else "toq" ) for i in range(1,11)]
+print(data)
+
+
+# 10-masala
+data = [100000, 250000, 50000, 300000]
+new = [(i * 1.1 if i > 200000 else i*1) for i in data]
+print(new)
+
+
+# 11-masala
+new = [(i, j) for i in range(1, 4) for j in range(1, 5)]
+print(new)
         
 
-# # 12-masala
-# matrix = [
-#     [1, 2, 3],
-#     [4, 5, 6],
-#     [7, 8, 9]
-# ]
+# 12-masala
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
 
-# new = [j for i in matrix for j in i]
-# print(new)
+new = [j for i in matrix for j in i]
+print(new)
 
 
-# # 13-masala
-# text = "Assalomu alaykum"
-# unlilar = ['a', 'i', 'e', 'o', 'u', 'A', 'I', 'E', 'O', 'U', "o'", "O'"]
+# 13-masala
+text = "Assalomu alaykum"
+unlilar = ['a', 'i', 'e', 'o', 'u', 'A', 'I', 'E', 'O', 'U', "o'", "O'"]
 
-# new = [i for i in text if i in unlilar]
-# print(new)
+new = [i for i in text if i in un lilar]
+print(new)
 
 
 
