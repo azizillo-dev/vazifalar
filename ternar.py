d35797b071862550b8e54571cff857960e275ab8
Azizillo <azizillonabiyev52@gmail.com>
2026-06-10 09:24:43 +0500

    commentariydan chiqarildi

--- ternar.py
+++ ternar.py
@@ -1,14 +1,14 @@
-# # 1-masala
-# n = int(input("n = "))
-# print("juft" if n % 2 == 0 else "toq")
+# 1-masala
+n = int(input("n = "))
+print("juft" if n % 2 == 0 else "toq")
 
-# # 2-masala
-# n = int(input("n = "))
-# print("musbat" if n > 0 else "manfiy" if n < 0 else "nol")
+# 2-masala
+n = int(input("n = "))
+print("musbat" if n > 0 else "manfiy" if n < 0 else "nol")
 
-# # 3-masala
-# yosh = int(input("yosh = "))
-# print("voyaga yetmagan" if yosh < 18 else "voyaga yetgan")
+# 3-masala
+yosh = int(input("yosh = "))
+print("voyaga yetmagan" if yosh < 18 else "voyaga yetgan")
 
 # 4-masala
 a = int(input("a = "))
