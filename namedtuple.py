93d2e6895409bb707fa65910f0737a4f728c85a1
Azizillo <azizillonabiyev52@gmail.com>
2026-06-14 17:19:41 +0500

    namedtuple boshlandi

---
+++ namedtuple.py
@@ -0,0 +1,22 @@
+from collections import namedtuple
+
+# Student = namedtuple("User", ["name", "age", "group"])
+
+# user1 = Student("Azizillo", 19, "310-25")
+# print(user1)
+# print(user1[2])
+# print(user1.name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
